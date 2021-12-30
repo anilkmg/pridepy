0c6fe9a3f2c48177c315fdb7ee475a122ff6d184
Anil Madugundu <madugundu.anil@mayo.edu>
2021-12-30 11:26:55 -0600

    Included original test_authentication with dummy credentials

---
+++ tests/test_authentication.py
@@ -0,0 +1,41 @@
+import unittest
+
+import pytest
+
+from pridepy.authentication import Authentication
+
+
+class TestAuthentication(unittest.TestCase):
+    """
+    A test class to test Authentication related methods.
+    """
+
+    @pytest.mark.skip(reason="Needs credentials")
+    def test_get_token(self):
+        """
+        Test get API AAP token functionality
+        :return:
+        """
+        username = "address@email.com"
+        password = "*******"
+        auth = Authentication()
+        api_token = auth.get_token(username, password)
+        print(api_token)
+        self.assertTrue(len(api_token) > 20, "Token not found!")
+
+    @pytest.mark.skip(reason="Needs credentials")
+    def test_validate_token(self):
+        """
+        Test get API AAP token is valid or expired
+        :return:
+        """
+        username = "address@email.com"
+        password = "*******"
+        auth = Authentication()
+        api_token = auth.get_token(username, password)
+        print(api_token)
+        self.assertTrue(auth.validate_token(api_token), "Token is invalid or expired!")
+
+
+if __name__ == '__main__':
+    unittest.main()
